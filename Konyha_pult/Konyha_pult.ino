eda25b7d0a0a3c4b37679c8705172e19424de48a
kalmanfi <kalmanfi@gmail.com>
2016-02-07 09:13:10 +0100

    HUM érzékelő megszakítással történő kezelése

--- Konyha_pult/Konyha_pult.ino
+++ Konyha_pult/Konyha_pult.ino
@@ -1,25 +1,20 @@
-#define HUM_pin   7  //Human detektor
+#define HUM_pin   2  //Human detektor
 #define REL_pin   8  //Relé
 
-bool LED;           //LED állapota
-bool HUM;           //Human detektor állapota
-int  T = 3;         //Időzítő
+volatile bool HUM;           //Human detektor állapota
+volatile int  T = 3;         //Időzítő
 
 void setup() {
-  noInterrupts()
   Serial.begin(9600);
   pinMode(REL_pin, OUTPUT);
   digitalWrite(REL_pin,HIGH);
   pinMode(HUM_pin, INPUT);
+  attachInterrupt(0, HUM_event,CHANGE);
 }
 
 void loop() {
-  bool HUM = digitalRead(HUM_pin);
-  if (HUM) T = 3;                           //Ha van mozgás, akkor időzítőviszzaállít 3-ra
-  else if (T) T--;                          //Ha nincs mozgás, és T>0 akkor T--
-  
-  if (T) digitalWrite(REL_pin,LOW);        //Ha T>0 Led ég
-  else digitalWrite(REL_pin,HIGH);           //Ha T=0 Led nem ég
+  if ((!HUM) & T>0) T--;                             //Ha nincs mozgás, akkor visszaszámol
+  if (!T) digitalWrite(REL_pin,HIGH);                //Ha T=0 LED kikapcsol          
 
   Serial.print("Move: ");
   Serial.print(HUM);
@@ -28,4 +23,16 @@ void loop() {
   Serial.print(" Led state: ");
   Serial.println(digitalRead(REL_pin));
   
-  delay(1000);      
+  delay(1000);
+}
+
+void HUM_event() {                     //Ha van mozgás, akkor időzítőviszzaállít 3-ra, LED fel
+  HUM = digitalRead(HUM_pin);
+  if (HUM) {
+    T = 3;
+    Serial.println("mozgás van"); 
+    digitalWrite(REL_pin,LOW);                        
+  }
+}
+
+
